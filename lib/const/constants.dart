302f5df2a20974e0f4b6fc18a7acee1228b93d88
Henrik Haeger <henrik.haeger@comma-soft.com>
2024-01-10 16:04:03 +0100

    month to double as a method

--- lib/const/constants.dart
+++ lib/const/constants.dart
@@ -13,3 +13,46 @@ const categoryTypes = [
   CategoryType.savings,
   CategoryType.income
 ];
+
+Widget getMonthTextByDouble(double m) {
+  String text = "";
+  switch (m.toInt()) {
+    case 1:
+      text = "Jan";
+      break;
+    case 2:
+      text = "Feb";
+      break;
+    case 3:
+      text = "Mar";
+      break;
+    case 4:
+      text = "Apr";
+      break;
+    case 5:
+      text = "May";
+      break;
+    case 6:
+      text = "Jun";
+      break;
+    case 7:
+      text = "Jul";
+      break;
+    case 8:
+      text = "Aug";
+      break;
+    case 9:
+      text = "Sep";
+      break;
+    case 10:
+      text = "Oct";
+      break;
+    case 11:
+      text = "Nov";
+      break;
+    case 12:
+      text = "Dec";
+      break;
+  }
+  return Text(text);
+}
